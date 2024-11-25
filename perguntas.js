e5581cc4a63726c363a1cad8c456beb8efa9e9d8
Mateuspot-2007 <138795304+Mateuspot-2007@users.noreply.github.com>
2024-11-25 08:56:11 -0300

    Add files via upload
    projeto finalizado.

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,23 @@
 criaCartao(
-    'Curiosidades',
-    'Qual a fruta mais calórica do mundo?',
-    'Abacate. Quatro colheres de sopa de abacate têm por volta de 160 kcal'
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
 )
 
 criaCartao(
-    'Capital',
-    'Qual a capital dos EUA?',
-    'Washington'
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
 )
 
 criaCartao(
-    'Ciễncia',
-    'Quanto tempo leva para a luz do sol chegar à terra?',
-    'Aproximadamente 9 minutos'
+    'Programação',
+    'O que é uma função?',
+    'Uma função na verdade é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Corpo humano',
-    'Quantos litros de sangue tem o corpo humano?',
-    'Aproximadamente 5 litros'
-)
-
-criaCartao(
-    'Tradução',
-    'Como se diz tanto faz na lingua Inglesa?',
-    'Se diz "Whatever" '
-)
-
-criaCartao(
-    'Tradução',
-    'Como se diz tchau na lingua Inglesa?',
-    'Se diz "Bye"'
-)
+    'Lingua inglesa',
+    'Como se diz PORCO em inglês?',
+    'PORCO em ingles é PIG'
+) 
